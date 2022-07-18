7a7e29f7aa9bec977a1d4b12b30bc0ccccf62ed1
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-07-18 17:11:05 +0200

    fruit in bdd

--- statsPirate.py
+++ statsPirate.py
@@ -14,3 +14,4 @@ class StatsPirate(object):
 		defense=10*level*(5-qualite)
 		fatigue=100*(5-qualite)
 		return [vie*(100+fruitpower[0])/100, degats*(100+fruitpower[1])/100, defense*(100+fruitpower[2])/100, fatigue*(100+fruitpower[3])/100]
+
